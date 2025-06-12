33808ec1f573b0b5de07135a1fd1d428d684d79f
aakashvishu120 <aakashvishu120@gmail.com>
2025-06-12 15:09:22 +0530

    added listing of employess, use of async, await and map fuction of ES6

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from './Table'
+// import Table from '../utils/MOCK_DATA.json'
+
 
 
 const Body = () => {
 
-   
+    const [data, setData] = useState([])
+
+    const fetchData = async () => {
+
+        try {
+            const response = await fetch('/MOCK_DATA.json');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const jsonData = await response.json();
+            console.log("this is a data coming from body")
+            console.log(jsonData)
+            setData(jsonData);
+        } catch (err) {
+            // setError(err.message);
+        } finally {
+            // setLoading(false);
+        }
+    };
 
 
 
     return (
         <div className=''>
             <div className='flex items-center justify-center'>
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Populate Data</button>
+                <button onClick={fetchData} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Populate Data</button>
             </div>
-            <Table />
+                
+                {/* if (data.length != 0){
+                    <Table/>
+                } */}
+
+                 {data.length != 0? <Table data={data} />: null }
         </div>
     )
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,78 +1,119 @@
 import React from 'react'
 
-const Table = () => {
+const Table = ({data}) => {
+
+    console.log("inside table jsx")
+    console.log(data)
+    // console.log(data[0])
+    // console.log("---------------")
+
     return (
         <div
-            class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
-            <table class="w-full text-left table-auto min-w-max">
+            className="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
+            <table className="w-full text-left table-auto min-w-max">
                 <thead>
                     <tr>
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">
                                 Emp. NO.
                             </p>
                         </th>
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">
                                 First Name
                             </p>
                         </th>
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">
                                 Last Name
                             </p>
                         </th>
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">Hourly Wage</p>
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">Hourly Wage</p>
                         </th>
 
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">
                                 Hour Worked
                             </p>
                         </th>
 
-                        <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased leading-none font-bold">
+                        <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased leading-none font-bold">
                                 Monthly Pay
                             </p>
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                         <td class="p-4 border-b border-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                               1
+                    {data.map((item, index) => (
+                        <tr>
+                         <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                               {item.id}
                             </p>
                         </td>
-                        <td class="p-4 border-b border-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                John 
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                {item.firstName}
                             </p>
                         </td>
-                        <td class="p-4 border-b border-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                michael
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                {item.lastName}
                             </p>
                         </td>
-                        <td class="p-4 border-b border-blue-gray-50">
-                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                              100
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                              {item.hourlyWage}
                             </p>
                         </td>
-                        <td class="p-4 border-b border-blue-gray-50">
-                            <a href="#" class="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <a href="#" className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
                                 240
                             </a>
                         </td>
 
-                        <td class="p-4 border-b border-blue-gray-50">
-                            <a href="#" class="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <a href="#" className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
                                 24000
                             </a>
                         </td>
                     </tr>
+                    ))}
+                    {/* <tr>
+                         <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                               1
+                            </p>
+                        </td>
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                John 
+                            </p>
+                        </td>
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                michael
+                            </p>
+                        </td>
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                              100
+                            </p>
+                        </td>
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <a href="#" className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+                                240
+                            </a>
+                        </td>
+
+                        <td className="p-4 border-b border-blue-gray-50">
+                            <a href="#" className="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
+                                24000
+                            </a>
+                        </td>
+                    </tr> */}
 
                 </tbody>
             </table>
